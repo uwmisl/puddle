fe11c3b7260d9198f3c58d8634e9a5f05805532b
Max Willsey <me@mwillsey.com>
2017-12-05 14:19:31 -0800

    Add more tests

--- tests/test_arch.py
+++ tests/test_arch.py
@@ -1,6 +1,6 @@
 import pytest
 
-from puddle.arch import Architecture, CollisionError
+from puddle.arch import Architecture, CollisionError, Droplet
 
 
 def test_arch_parse(arch_path):
@@ -13,12 +13,44 @@ def test_arch_parse(arch_path):
     assert Architecture.from_file(arch_path)
 
 
-def test_collision(session01):
-    """ Test to make sure that inputting droplets next to each other fails."""
+def test_droplet_copy():
+    a = Droplet(info='a', location=(1,1))
+    a2 = a.copy()
+    # copy gets you a fresh collision group
+    assert a.collision_group != a2.collision_group
 
-    session01.input_droplet((3,1))
+
+def test_add_droplet(arch01):
+    arch = arch01
+
+    a = Droplet(info='a', location=(1,1))
+    b = Droplet(info='b', location=(3,3))
+    c = Droplet(info='c', location=(3,5))
+
+    # these are overlapping and too close, respectively
+    b2 = Droplet(info='b2', location=(3,3))
+    c2 = Droplet(info='c2', location=(4,5))
+
+    # this one should be okay to overlap with b
+    b_ok = Droplet('b_ok', (3,3))
+    b_ok.collision_group = b.collision_group
+
+    arch.add_droplet(a)
+    arch.add_droplet(b)
+    arch.add_droplet(c)
+    assert len(arch.droplets) == 3
+
+    # these should fail harmlessly
     with pytest.raises(CollisionError):
-        session01.input_droplet((3,2))
+        arch.add_droplet(b2)
+    with pytest.raises(CollisionError):
+        arch.add_droplet(c2)
+
+    # caught errors shouldn't modify the droplets set
+    assert len(arch.droplets) == 3
+
+    # this one should be added as normal
+    arch.add_droplet(b_ok)
 
 
 def test_mix(session01):
@@ -29,6 +61,7 @@ def test_mix(session01):
     b = session.input_droplet((3,3), info='b')
 
     ab = session.mix(a, b)
+    assert len(session.arch.droplets) == 1
     assert ab.info == '(a, b)'
 
 
@@ -36,8 +69,9 @@ def test_split(session01):
 
     session = session01
 
-    a = session.input_droplet((0,0))
-    b = session.input_droplet((3,3))
+    a = session.input_droplet((0,0), info='a')
+    session.input_droplet((3,3), info='b')
 
-    session.split(b)
+    a1, a2 = session.split(a)
     assert len(session.arch.droplets) == 3
+    assert a1.info == a2.info == 'a'
