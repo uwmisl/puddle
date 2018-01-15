b616ac049732adbc7e09beea2b2f7a44d99b8a76
Michal Piszczek <michalp@cs.washington.edu>
2018-01-15 00:20:07 -0800

    Prepare engine for rewrite

--- puddle/engine.py
+++ puddle/engine.py
@@ -19,51 +19,60 @@ from puddle.execution import Execution
 # todo: an actual dag using nx
 # todo: optimizations on said dag
 
+# todo: rewrite to deal with mutliple bindings via soft_bind
+
 class Engine:
 
     def __init__(self, execution: Execution) -> None:
         self.execution = execution
         # Associates droplet ids to their source commands
         self.dependencies = {}
+        self.commands = []
 
     def virtualize(self, command: Command) -> Any:
-
-        for droplet in command.output_droplets:
-            self.dependencies[droplet._id] = command
+        # for droplet in command.output_droplets:
+        #     self.dependencies[droplet._id] = command
+        self.commands.append(command)
 
         return command.output_droplets
 
     def realize(self, command: Command) -> Any:
+        for cmd in self.commands:
+            self.execution.go(cmd)
+        self.commands = []
 
-        # BFS up the DAG to get a valid execution order.
+        # # BFS up the DAG to get a valid execution order.
 
-        visited = [command]
+        # visited = [command]
 
-        for cmd in visited:
-            for droplet in cmd.input_droplets:
-                if droplet._is_virtual and droplet._id in self.dependencies:
-                        dependency = self.dependencies[droplet._id]
-                        visited.append(dependency)
+        # for cmd in visited:
+        #     for droplet in cmd.input_droplets:
+        #         if droplet._is_virtual and droplet._id in self.dependencies:
+        #                 dependency = self.dependencies[droplet._id]
+        #                 visited.append(dependency)
 
-        # make sure visited is unique so we don't do anything twice
-        assert len(set(visited)) == len(visited)
+        # # make sure visited is unique so we don't do anything twice
+        # assert len(set(visited)) == len(visited)
 
-        for cmd in reversed(visited):
-            # Only execute commands with non-virtual outputs.
-            if all(d._is_virtual for d in cmd.output_droplets):
-                self.execution.go(cmd)
+        # for cmd in reversed(visited):
+        #     # Only execute commands with non-virtual outputs.
+        #     if all(d._is_virtual for d in cmd.output_droplets):
+        #         self.execution.go(cmd)
 
         return command.output_droplets
 
     def flush(self, droplet=None) -> None:
-        if droplet is None:
-            # flush all
-            for droplet_id in self.dependencies:
-                cmd = self.dependencies[droplet_id]
-                if all(d._is_virtual for d in cmd.output_droplets):
-                    self.realize(cmd)
-        else:
-            # only evaluate dependencies for given droplet
-            cmd = self.dependencies[droplet._id]
-            if all(d._is_virtual for d in cmd.output_droplets):
-                self.realize(cmd)
+        for cmd in self.commands:
+            self.execution.go(cmd)
+        self.commands = []
+        # if droplet is None:
+        #     # flush all
+        #     for droplet_id in self.dependencies:
+        #         cmd = self.dependencies[droplet_id]
+        #         if all(d._is_virtual for d in cmd.output_droplets):
+        #             self.realize(cmd)
+        # else:
+        #     # only evaluate dependencies for given droplet
+        #     cmd = self.dependencies[droplet._id]
+        #     if all(d._is_virtual for d in cmd.output_droplets):
+        #         self.realize(cmd)
