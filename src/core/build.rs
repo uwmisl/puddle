65170fbed8ecf333768725d6e4318d9d954c6798
Max Willsey <me@mwillsey.com>
2018-06-22 10:05:19 -0700

    Use better background subtraction

--- src/core/build.rs
+++ src/core/build.rs
@@ -9,6 +9,7 @@ fn main() {
         println!("cargo:rustc-link-lib=opencv_core");
         println!("cargo:rustc-link-lib=opencv_imgproc");
         println!("cargo:rustc-link-lib=opencv_imgcodecs");
+        println!("cargo:rustc-link-lib=opencv_video");
         println!("cargo:rustc-link-lib=opencv_videoio");
         println!("cargo:rustc-link-lib=opencv_highgui");
 
--- src/core/src/vision/droplet_detect.cpp
+++ src/core/src/vision/droplet_detect.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/videoio/videoio.hpp"
+#include "opencv2/video/background_segm.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
 #include <iostream>
@@ -79,12 +80,8 @@ typedef struct {
   Mat *current;
 
   // the base frame to difference between
-  Mat *background;
+  Ptr<BackgroundSubtractor> bg_subber;
 
-  // detection parameters
-  int erode1;
-  int dilate1;
-  int erode2sub;
 } DetectionArgs;
 
 
@@ -95,54 +92,6 @@ void detect_droplets(int value, void* args_p) {
   UNUSED(value);
   DetectionArgs *args = (DetectionArgs *)args_p;
 
-  Mat diff;
-  absdiff(*args->current, *args->background, diff);
-  imshow("diff", diff);
-
-  Mat diffThresh;
-  threshold(diff, diffThresh, 30, 255, THRESH_BINARY);
-  imshow("diffThresh", diffThresh);
-
-	// Erode the image to get rid of noise
-  Mat erodedImg;
-	// dilate(diffThresh, erodedImg,
-  //        getStructuringElement(MORPH_ELLIPSE, Size(args->k_size1, args->k_size1)));
-	erode(diffThresh, erodedImg,
-        getStructuringElement(MORPH_ELLIPSE, Size(args->erode1, args->erode1)));
-  imshow("eroded1", erodedImg);
-	dilate(erodedImg, erodedImg,
-         getStructuringElement(MORPH_ELLIPSE, Size(args->dilate1, args->dilate1)));
-  // imshow("eroded2", erodedImg);
-  int dim = args->dilate1 + args->erode2sub - 20;
-	erode(erodedImg, erodedImg,
-        getStructuringElement(MORPH_ELLIPSE, Size(dim, dim)));
-  imshow("eroded3", erodedImg);
-
-	// Find all the contours in the image, and filter them
-	vector< vector<Point> > contours;
-  findContours(erodedImg, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	vector< vector<Point> > filteredContours;
-	for(unsigned i = 0; i<contours.size(); i++){
-    RotatedRect r = minAreaRect(Mat(contours[i]));
-		int h = r.size.height;
-		int w = r.size.width;
-    double area = contourArea(contours[i]);
-		if (w != 0 && h != 0 &&
-        w / h < 9 && h / w < 9 &&
-        50 < area && area < 20000) {
-			filteredContours.push_back(contours[i]);
-			// cout << contours[i] << endl;
-		}
-	}
-
-	int n_contours = filteredContours.size();
-	cout << "Found " << n_contours << " countours" << endl;
-
-  Mat colored;
-  cvtColor(*args->current, colored, CV_GRAY2BGR);
-  Scalar color(0,0,255);
-  drawContours( colored, filteredContours, -1, color, 2);
-  imshow("Colored", colored);
 }
 
 extern "C"
@@ -151,23 +100,7 @@ int detect_from_files(char* currentPath, char* backgroundPath) {
   Mat currentFrame = readGray(currentPath);
   Mat backgroundFrame = readGray(backgroundPath);
 
-  namedWindow("window");
-
-  DetectionArgs args;
-  args.background = &backgroundFrame;
-  args.current = &currentFrame;
-  args.erode1 = 3;
-  args.dilate1 = 50;
-  args.erode2sub = 20;
-
-  createTrackbar("Erode Size 1", "window", &args.erode1, 10, &detect_droplets, (void*)&args);
-  createTrackbar("Dilate Size 1", "window", &args.dilate1, 200, &detect_droplets, (void*)&args);
-  createTrackbar("Erode Sub 2 - 20", "window", &args.erode2sub, 40, &detect_droplets, (void*)&args);
-
-  // make an initial callback
-  detect_droplets(0, (void*)&args);
-
-  // don't worry about markers for now
+  cout << "WARNING: THIS IS UNIMPLEMENTED FOR NOW!" << endl;
 
 	vector<Point> squareFiducial = find_fiducial(currentFrame, 20000, 4);
 	vector<Point> pentagonFiducial = find_fiducial(currentFrame, 20000, 5);
@@ -192,48 +125,66 @@ void detect_from_camera() {
   // cap.set(CAP_PROP_BUFFERSIZE, 1);
   // cap.set(CAP_PROP_POS_FRAMES, 5);
 
-  Mat backgroundFrame;
-  cap.read(backgroundFrame);
-  Mat backgroundFrameGray;
-  cvtColor(backgroundFrame, backgroundFrameGray, CV_RGB2GRAY);
-
   Mat currentFrame;
-  cap.read(currentFrame);
   Mat currentFrameGray;
-  cvtColor(currentFrame, currentFrameGray, CV_RGB2GRAY);
 
-  DetectionArgs args;
-  args.background = &backgroundFrameGray;
-  args.current = &currentFrameGray;
-  args.erode1 = 3;
-  args.dilate1 = 20;
-  args.erode2sub = 5;
+  // int history = 500;
+  // double dist2Threshold = 400.0;
+  // bool detectShadows = false;
+  // Ptr<BackgroundSubtractorKNN> bg = createBackgroundSubtractorKNN(history, dist2Threshold, detectShadows);
 
-  createTrackbar("Erode Size 1", "window", &args.erode1, 10, &detect_droplets, (void*)&args);
-  createTrackbar("Dilate Size 1", "window", &args.dilate1, 200, &detect_droplets, (void*)&args);
-  createTrackbar("Erode Sub 2 - 20", "window", &args.erode2sub, 40, &detect_droplets, (void*)&args);
+  int history = 500;
+  double varThreshold = 16;
+  bool detectShadows = false;
+  Ptr<BackgroundSubtractor> bgSub = createBackgroundSubtractorMOG2(history, varThreshold, detectShadows);
 
-  detect_droplets(0, (void*)&args);
-
-  for (unsigned i = 0; ; i++) {
-    cout << "Loop " << i << endl;
-    switch ((char)waitKey(0)) {
-    case 'q': goto done;
-    case 'c':
-      cap.read(backgroundFrame);
-      cvtColor(backgroundFrame, backgroundFrameGray, CV_RGB2GRAY);
-      break;
-    }
+  int i = 0;
+  while (waitKey(10) != 'q') {
+    cout << "Loop " << i++ << endl;
 
     cap.read(currentFrame);
+    imshow("current", currentFrame);
     cvtColor(currentFrame, currentFrameGray, CV_RGB2GRAY);
 
     vector<Point> squareFiducial = find_fiducial(currentFrame, 20000, 4);
     vector<Point> pentagonFiducial = find_fiducial(currentFrame, 20000, 5);
 
-    // re-run detection
-    detect_droplets(0, (void*)&args);
-  }
+    blur(currentFrameGray, currentFrameGray, Size(3,3));
 
- done:;
+    // get the fg and bg
+    Mat fg;
+    bgSub->apply(currentFrameGray, fg);
+    imshow("foreground", fg);
+
+    Mat bg;
+    bgSub->getBackgroundImage(bg);
+    imshow("background", bg);
+
+    // Find all the contours in the image, and filter them
+    vector< vector<Point> > contours;
+    findContours(fg, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    vector< vector<Point> > filteredContours;
+    for(unsigned i = 0; i<contours.size(); i++){
+      RotatedRect r = minAreaRect(Mat(contours[i]));
+      int h = r.size.height;
+      int w = r.size.width;
+      double area = contourArea(contours[i]);
+      if (w != 0 && h != 0 &&
+          w / h < 9 && h / w < 9 &&
+          50 < area && area < 20000) {
+        filteredContours.push_back(contours[i]);
+        // cout << contours[i] << endl;
+      }
+    }
+
+    int n_contours = filteredContours.size();
+    cout << "Found " << n_contours << " countours" << endl;
+
+    // draw the contours
+    Mat colored;
+    cvtColor(currentFrameGray, colored, CV_GRAY2BGR);
+    Scalar color(0,0,255);
+    drawContours( colored, filteredContours, -1, color, 2);
+    imshow("Colored", colored);
+  }
 }
