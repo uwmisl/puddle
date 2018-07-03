e7c905f64d0684dff487c17e3a0b42e7667e4c85
Max Willsey <me@mwillsey.com>
2018-07-03 09:51:54 -0700

    Add a "move in a circle" test to pi-test
    Useful for tightening the board

--- src/core/src/bin/pi-test.rs
+++ src/core/src/bin/pi-test.rs
@@ -7,6 +7,8 @@ extern crate log;
 use clap::{App, Arg, SubCommand};
 use std::error::Error;
 use std::fs::File;
+use std::time::Duration;
+use std::thread;
 
 use puddle_core::grid::{Droplet, DropletId, Grid, Location, Snapshot};
 use puddle_core::pi::RaspberryPi;
@@ -40,7 +42,18 @@ fn main() -> Result<(), Box<Error>> {
             SubCommand::with_name("set-loc")
                 .arg(Arg::with_name("gridpath").takes_value(true).required(true))
                 .arg(Arg::with_name("y").takes_value(true).required(true))
-                .arg(Arg::with_name("x").takes_value(true).required(true)),
+                .arg(Arg::with_name("x").takes_value(true).required(true))
+                .arg(Arg::with_name("height").takes_value(true).default_value("1"))
+                .arg(Arg::with_name("width").takes_value(true).default_value("1")),
+        )
+        .subcommand(
+            SubCommand::with_name("circle")
+                .arg(Arg::with_name("gridpath").takes_value(true).required(true))
+                .arg(Arg::with_name("y").takes_value(true).required(true))
+                .arg(Arg::with_name("x").takes_value(true).required(true))
+                .arg(Arg::with_name("height").takes_value(true).default_value("2"))
+                .arg(Arg::with_name("width").takes_value(true).default_value("2"))
+                .arg(Arg::with_name("sleep").takes_value(true).default_value("1000")),
         )
         .get_matches();
 
@@ -70,6 +83,8 @@ fn main() -> Result<(), Box<Error>> {
             let gridpath = m.value_of("gridpath").unwrap();
             let y = m.value_of("y").unwrap().parse().unwrap();
             let x = m.value_of("x").unwrap().parse().unwrap();
+            let height = m.value_of("height").unwrap().parse().unwrap();
+            let width = m.value_of("width").unwrap().parse().unwrap();
             let mut droplets = Map::new();
             let id = DropletId {
                 id: 0,
@@ -78,7 +93,7 @@ fn main() -> Result<(), Box<Error>> {
             let droplet = Droplet {
                 id: id,
                 location: Location { y, x },
-                dimensions: Location { y: 1, x: 1 },
+                dimensions: Location { y: height, x: width },
                 volume: 1.0,
                 destination: None,
                 collision_group: 0,
@@ -95,6 +110,64 @@ fn main() -> Result<(), Box<Error>> {
 
             pi.init_hv507();
             pi.output_pins(&grid, &snapshot);
+            Ok(())
+        }
+        ("circle", Some(m)) => {
+            let gridpath = m.value_of("gridpath").unwrap();
+            let y = m.value_of("y").unwrap().parse().unwrap();
+            let x = m.value_of("x").unwrap().parse().unwrap();
+            let height = m.value_of("height").unwrap().parse().unwrap();
+            let width = m.value_of("width").unwrap().parse().unwrap();
+            let duration = Duration::from_millis(m.value_of("sleep").unwrap().parse().unwrap());
+
+            let mut droplets = Map::new();
+            let id = DropletId {
+                id: 0,
+                process_id: 0,
+            };
+            let droplet = Droplet {
+                id: id,
+                location: Location { y, x },
+                dimensions: Location { y: 1, x: 1 },
+                volume: 1.0,
+                destination: None,
+                collision_group: 0,
+            };
+            info!("Using {:#?}", droplet);
+            droplets.insert(id, droplet);
+            let mut snapshot = Snapshot {
+                droplets: droplets,
+                commands_to_finalize: vec![],
+            };
+
+            let reader = File::open(gridpath)?;
+            let grid = Grid::from_reader(reader)?;
+
+            pi.init_hv507();
+            pi.output_pins(&grid, &snapshot);
+
+            let mut set_loc = |yo, xo| {
+                let loc = Location {y: y + yo, x: x + xo};
+                snapshot.droplets.get_mut(&id).unwrap().location = loc;
+                pi.output_pins(&grid, &snapshot);
+                println!("Droplet at {}", loc);
+                thread::sleep(duration);
+            };
+            loop {
+                for xo in 0..width {
+                    set_loc(xo, 0);
+                }
+                for yo in 0..height {
+                    set_loc(width-1, yo);
+                }
+                for xo in 0..width {
+                    set_loc(width-1-xo, height-1);
+                }
+                for yo in 0..height {
+                    set_loc(0, height-1-yo);
+                }
+            }
+
             Ok(())
         }
         _ => {
