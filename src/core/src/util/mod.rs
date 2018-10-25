1862f16d47f5c1a8a92588e3863bf0e18df517ba
Max Willsey <me@mwillsey.com>
2018-10-25 15:05:44 -0700

    Add a simple duplicate checker to util

--- src/core/src/util/mod.rs
+++ src/core/src/util/mod.rs
@@ -58,3 +58,38 @@ pub fn seconds_duration(seconds: f64) -> Duration {
     let nanos = seconds.fract() * 1e9;
     Duration::new(secs as u64, nanos as u32)
 }
+
+pub fn find_duplicate<T>(items: &[T]) -> Option<(usize, usize)>
+where
+    T: PartialEq,
+{
+    let len = items.len();
+    for i in 0..len {
+        for j in (i + 1)..len {
+            if items[i] == items[j] {
+                return Some((i, j));
+            }
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_duplicate() {
+        let a1 = &[1, 2, 3, 1];
+        assert_eq!(find_duplicate(a1), Some((0, 3)));
+
+        let a2 = &[1, 2, 2, 1];
+        assert_eq!(find_duplicate(a2), Some((0, 3)));
+
+        let a3 = &[1, 1, 1, 1];
+        assert_eq!(find_duplicate(a3), Some((0, 1)));
+
+        let a4 = &[1, 2, 3, 4];
+        assert_eq!(find_duplicate(a4), None);
+    }
+}
