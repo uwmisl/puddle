c39e8d2b6f2a4672c61de8d4d37762309dcb32b7
Max Willsey <me@mwillsey.com>
2019-02-26 11:22:11 -0800

    Use nix to build stuff

---
+++ default.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> { 
+  overlays = [ 
+    # set up the rust overlay for up-to-date rust versions
+    (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
+  ];
+};
+let
+  # using rust overlay
+  rustChannel = rustChannelOf { channel = "1.32.0"; };
+  rust = rustChannel.rust.override {
+    extensions = ["rust-src"];
+    targets = [
+      "x86_64-unknown-linux-gnu"
+      "armv7-unknown-linux-musleabihf"
+    ];
+  };
+
+  # don't install arm stuff on travis
+  in_ci = builtins.getEnv "CI" == "true";
+  arm = import <nixpkgs> { crossSystem.config = "armv7l-unknown-linux-musleabihf"; };
+in
+stdenv.mkDerivation {
+  name = "puddle";
+  buildInputs = [
+    (if in_ci then null else arm.stdenv.cc)
+    rust
+    rustracer
+  ];
+  RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/src";
+}
--- src/core/sync.sh
+++ src/core/sync.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ev
 
-pi=bananapi
+pi=blueberry-pie.zt
 
-rsync='rsync --compress --times --progress'
+rsync='rsync -iP --compress-level=9'
+
+target='armv7-unknown-linux-musleabihf'
 
 # compile
-# cross build --target armv7-unknown-linux-gnueabihf --features pi,vision
-cross build --target armv7-unknown-linux-gnueabihf --features pi
+cargo build --target $target --features pi
 
 # kill any running servers
-ssh $pi -- killall -q puddle-server || true
+# ssh $pi -- killall -q puddle-server || true
 
 # sync the binaries
-$rsync target/armv7-unknown-linux-gnueabihf/debug/{vision-test,puddle-server,pi-test} $pi:
+# $rsync target/$target/debug/{vision-test,puddle-server,pi-test} $pi:
+$rsync target/$target/debug/{puddle-server,pi-test} $pi:
 
 # sync the python
-$rsync ../python/puddle.py $pi:puddle/src/python/
-$rsync ../python/examples/*.py $pi:puddle/src/python/examples/
+# $rsync ../python/puddle.py $pi:puddle/src/python/
+# $rsync ../python/examples/*.py $pi:puddle/src/python/examples/
 
 # sync the boards
-$rsync ../../tests/arches/*.json $pi:puddle/tests/arches/
+$rsync --relative ../../tests/./arches/*.json $pi:
