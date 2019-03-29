124a747eb63dbaf52e32f81c40b91b936c16f35a
Max Willsey <me@mwillsey.com>
2019-03-29 09:36:23 -0700

    Get rid of some old code

--- src/core/src/bin/pi-test.rs
+++ src/core/src/bin/pi-test.rs
@@ -38,8 +38,6 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 enum SubCommand {
-    // Dac,
-    // Pwm,
     #[structopt(name = "set-polarity")]
     SetPolarity {
         frequency: f64,
@@ -84,6 +82,8 @@ enum SubCommand {
         )]
         stagger: Option<MyDuration>,
     },
+    // Dac,
+    // Pwm,
     // Temp,
     // Heat,
     // Pins,
@@ -256,115 +256,3 @@ fn mk_snapshot(blobs: &[SimpleBlob]) -> (Vec<DropletId>, Snapshot) {
     };
     (ids, snapshot)
 }
-
-// fn set_loc(m: &ArgMatches, pi: &mut RaspberryPi) -> Result<(), Box<Error>> {
-//     let grid = mk_grid(m)?;
-//     let location = m.value_of("location").unwrap().parse()?;
-//     let dimensions = m.value_of("dimensions").unwrap().parse()?;
-//     let (_, snapshot) = mk_snapshot(location, dimensions);
-//     pi.output_pins(&grid, &snapshot);
-//     Ok(())
-// }
-
-// fn circle(m: &ArgMatches, pi: &mut RaspberryPi) -> Result<(), Box<Error>> {
-//     let grid = mk_grid(m)?;
-
-//     let location = m.value_of("location").unwrap().parse()?;
-//     let dimensions = m.value_of("dimensions").unwrap().parse()?;
-//     let (id, mut snapshot) = mk_snapshot(location, dimensions);
-
-//     let size: Location = m.value_of("circle").unwrap().parse()?;
-//     let duration = Duration::from_millis(m.value_of("sleep").unwrap().parse()?);
-
-//     pi.output_pins(&grid, &snapshot);
-
-//     let mut set = |yo, xo| {
-//         let loc = Location {
-//             y: location.y + yo,
-//             x: location.x + xo,
-//         };
-//         snapshot.droplets.get_mut(&id).unwrap().location = loc;
-//         pi.output_pins(&grid, &snapshot);
-//         println!("Droplet at {}", loc);
-//         thread::sleep(duration);
-//     };
-
-//     loop {
-//         for xo in 0..size.x {
-//             set(xo, 0);
-//         }
-//         for yo in 0..size.y {
-//             set(size.x - 1, yo);
-//         }
-//         for xo in 0..size.x {
-//             set(size.x - 1 - xo, size.y - 1);
-//         }
-//         for yo in 0..size.y {
-//             set(0, size.y - 1 - yo);
-//         }
-//     }
-// }
-
-// fn heat(m: &ArgMatches, pi: &mut RaspberryPi) -> Result<(), Box<Error>> {
-//     let grid = mk_grid(&m)?;
-//     let heater_loc = m.value_of("heater").unwrap().parse()?;
-//     let temp = m.value_of("temp").unwrap().parse()?;
-//     let seconds = m.value_of("seconds").unwrap().parse()?;
-
-//     let heater = grid
-//         .get_cell(&heater_loc)
-//         .cloned()
-//         .unwrap()
-//         .peripheral
-//         .expect("Given location wasn't a heater!");
-//     let duration = seconds_duration(seconds);
-
-//     pi.heat(&heater, temp, duration)?;
-
-//     Ok(())
-// }
-
-// fn get_pin(pin: u32, grid: &Grid) -> Option<Location> {
-//     for (loc, electrode) in grid.locations() {
-//         if electrode.pin == pin {
-//             return Some(loc);
-//         }
-//     }
-//     None
-// }
-
-// fn test_pins(m: &ArgMatches, pi: &mut RaspberryPi) -> Result<(), Box<Error>> {
-//     let grid = mk_grid(&m)?;
-//     let millis = m.value_of("millis").unwrap().parse()?;
-//     let duration = if millis == 0 {
-//         println!("Press enter to step to next pin");
-//         None
-//     } else {
-//         Some(Duration::from_millis(millis))
-//     };
-//     let pin_limit = grid.max_pin() + 1;
-
-//     let mut stdin = io::stdin();
-//     let mut stdout = io::stdout();
-
-//     for i in 0..pin_limit {
-//         if let Some(loc) = get_pin(i, &grid) {
-//             println!("pin {} at {}", i, loc);
-//             let (_, snapshot) = mk_snapshot(loc, Location { y: 1, x: 1 });
-//             pi.output_pins(&grid, &snapshot);
-//         } else {
-//             println!("pin {} has no location", i);
-//         }
-
-//         // either wait or pause
-//         if let Some(duration) = duration {
-//             thread::sleep(duration);
-//         } else {
-//             print!("Outputting pin {}", i);
-//             stdout.flush().unwrap();
-//             let _ = stdin.read(&mut [0u8]).unwrap();
-//         }
-//     }
-
-//     Ok(())
-// }
